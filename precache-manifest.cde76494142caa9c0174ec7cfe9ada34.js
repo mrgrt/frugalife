cde0e2ba5a7816345f4f88960d1e2fab3277dc4a
Grahame Thomson <mrgrahamethomson@gmail.com>
2020-03-13 08:17:45 -0400

    Updates

---
+++ precache-manifest.cde76494142caa9c0174ec7cfe9ada34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fbb4cfe1a70a7362b8d38f00e9c7fa",
+    "url": "/frugalife/index.html"
+  },
+  {
+    "revision": "3a72dea7957b62463d81",
+    "url": "/frugalife/static/css/main.55e69184.chunk.css"
+  },
+  {
+    "revision": "d259c0037a0391491ff8",
+    "url": "/frugalife/static/js/2.80b5d3e9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/frugalife/static/js/2.80b5d3e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a72dea7957b62463d81",
+    "url": "/frugalife/static/js/main.99e71fb3.chunk.js"
+  },
+  {
+    "revision": "e0deeadb591bf44a6d4e",
+    "url": "/frugalife/static/js/runtime-main.334bc5af.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frugalife/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frugal/precache-manifest.e1114e51ec7a0e9679bf7d4461f1f532.js"
+  "/frugalife/precache-manifest.cde76494142caa9c0174ec7cfe9ada34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frugal/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frugalife/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
